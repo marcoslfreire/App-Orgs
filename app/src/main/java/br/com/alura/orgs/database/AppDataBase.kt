251fbff4217e4baee5ffd11937954efb0b4406d9
Marcos <mlfcosta@rd.com.br>
2023-05-20 02:00:31 -0300

    ajustes menu

--- app/src/main/java/br/com/alura/orgs/database/AppDataBase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDataBase.kt
@@ -6,12 +6,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import br.com.alura.orgs.database.converter.Converter
 import br.com.alura.orgs.database.converters.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
 import br.com.alura.orgs.model.Produto
 
-@Database(entities = [Produto::class], version = 1)
+@Database(entities = [Produto::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDataBase: RoomDatabase() {
     abstract fun produtoDao(): ProdutoDao
